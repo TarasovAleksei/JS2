ef298bfcf6c7681eeecf932ff08d9abe5e5d8305
TarasovAleksei <71583511+TarasovAleksei@users.noreply.github.com>
2021-01-27 18:26:39 +0300

    Lesson 2

--- main.js
+++ main.js
@@ -1,25 +1,52 @@
-const basket = {
-    products: [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500}
-    
-],
-
-  renderProducts() {
-    let productList = document.querySelector('.products');
-    this.products.forEach((products) => productList.insertAdjacentHTML('beforeend',
-        `<div class="product-item">
-        <h3>${products.title}</h3>
-        <p>${products.price}</p>
-        <button class="by-btn">Add to Cart</button>
-      </div>`))
-  }
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.totalPrice = 0;
+        this.total();
+    }
+    total() {
+        for (let item of this.goods) {
+            this.totalPrice += item.price;
+        }
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Нотебук', price: 2000},
+            {id: 2, title: 'Мыша', price: 20},
+            {id: 3, title: 'Клава', price: 200},
+            {id: 4, title: 'гампаД', price: 50},
+        ];
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        const totalEl = document.querySelector(".total");
+        for(let product of this.goods) {
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend',productObj.render());
+        }
+        totalEl.innerHTML = `<div class="total-price">
+                <span>Итоговая цена: ${this.totalPrice}</span>
+                </div>`
+    }
 }
 
-basket.renderProducts();
-
-
-
-
+class ProductItem {
+    constructor(product,img='https://placehold.it/200x150'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src ="${this.img}" alt="some img">
+                <h3 class="item-name">${this.title}</h3>
+                <p class="item-price">Цена: ${this.price}</p>
+                <button class="buy-btn">Купить</button>
+                </div>`
+    }
+}
+let list = new ProductList();
+list.render(); 
