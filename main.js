943dce66c7ba2f7f2324b12c806ad3f7f3bc7d32
TarasovAleksei <71583511+TarasovAleksei@users.noreply.github.com>
2021-02-03 15:38:08 +0300

    lesson4

--- main.js
+++ main.js
@@ -1,52 +1,7 @@
-class ProductList {
-    constructor(container = '.products') {
-        this.container = container;
-        this.goods = [];
-        this._fetchProducts();
-        this.totalPrice = 0;
-        this.total();
-    }
-    total() {
-        for (let item of this.goods) {
-            this.totalPrice += item.price;
-        }
-    }
-    _fetchProducts(){
-        this.goods = [
-            {id: 1, title: 'Нотебук', price: 2000},
-            {id: 2, title: 'Мыша', price: 20},
-            {id: 3, title: 'Клава', price: 200},
-            {id: 4, title: 'гампаД', price: 50},
-        ];
-    }
-    render(){
-        const block = document.querySelector(this.container);
-        const totalEl = document.querySelector(".total");
-        for(let product of this.goods) {
-            const productObj = new ProductItem(product);
-            block.insertAdjacentHTML('beforeend',productObj.render());
-        }
-        totalEl.innerHTML = `<div class="total-price">
-                <span>Итоговая цена: ${this.totalPrice}</span>
-                </div>`
-    }
-}
+const a = "тут написан 'какой-то' совершенно 'бредовый' текст aren't и все";
+const b = a.replace(/(\b'\b)|'/g, function (p0, p1) {
+        return p1 ? "'" : '"';
+    });
 
-class ProductItem {
-    constructor(product,img='https://placehold.it/200x150'){
-        this.title = product.title;
-        this.id = product.id;
-        this.price = product.price;
-        this.img = img;
-    }
-    render() {
-        return `<div class="product-item" data-id="${this.id}">
-                <img src ="${this.img}" alt="some img">
-                <h3 class="item-name">${this.title}</h3>
-                <p class="item-price">Цена: ${this.price}</p>
-                <button class="buy-btn">Купить</button>
-                </div>`
-    }
-}
-let list = new ProductList();
-list.render(); 
+console.log(b);
+
